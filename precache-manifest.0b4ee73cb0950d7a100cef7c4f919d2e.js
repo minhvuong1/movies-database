39829e4c8d769dd93be9e1a811a92e9b86214ea6
Minh Vuong <minhvuong@live.com>
2020-06-22 13:41:46 +1000

    Updates

---
+++ precache-manifest.0b4ee73cb0950d7a100cef7c4f919d2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "850d48f30820a016fd4ab98c3c9c8fe2",
+    "url": "/movies-database/index.html"
+  },
+  {
+    "revision": "e3d9307362b65eee6bf3",
+    "url": "/movies-database/static/css/main.f11fe894.chunk.css"
+  },
+  {
+    "revision": "fe4dec88b4311d4cc60d",
+    "url": "/movies-database/static/js/2.fd08b29f.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/movies-database/static/js/2.fd08b29f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e3d9307362b65eee6bf3",
+    "url": "/movies-database/static/js/main.c45de1de.chunk.js"
+  },
+  {
+    "revision": "65d4e21f017caab314d2",
+    "url": "/movies-database/static/js/runtime-main.7a13ae67.js"
+  }
+]);
